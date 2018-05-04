3c6a1b01ceb4cf3520c578e78bc2857745e98422
Keith <keithwhisper@gmail.com>
2018-05-04 15:16:39 +0800

    update: add manifest

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ const path = require('path')
 const Koa = require('koa')
 const BodyParser = require('koa-body')
 const views = require('koa-views')
+const serve = require('koa-static')
 
 const router = require('./routes/index')
 
 const app = new Koa()
 
-app.use(
-  views(path.join(__dirname, './views'), {
-    extension: 'pug',
-  }),
-)
+app
+  .use(
+    views(path.join(__dirname, './views'), {
+      extension: 'pug',
+    }),
+  )
+  .use(serve('./public'))
 
 app.use(BodyParser())
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(3000, () => {
+app.listen(8095, () => {
   console.log('server')
 })
