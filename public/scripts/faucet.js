7b9843e37370d6e769298807e5969ff6648100b6
Yao <GyonGyon@users.noreply.github.com>
2018-07-31 11:07:34 +0800

    update: change api
    neuron.getAccount() --> neuronAccount.getAccount()

--- public/scripts/faucet.js
+++ public/scripts/faucet.js
@@ -1,7 +1,7 @@
 var input = document.getElementById('id-input-address')
 
 try {
-  input.value = neuron.getAccount()
+  input.value = neuronAccount.getAccount()
 } catch (err) {
   console.error(err)
 }
